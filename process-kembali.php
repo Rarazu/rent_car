586b45a54e5706ec53cd46a77bf2b4ffac902c06
Rarazu <rara_zahra_29rpl@student.smktelkom-mlg.sch.id>
2021-11-11 10:38:51 +0700

    masih banyak yang error

--- process-kembali.php
+++ process-kembali.php
@@ -1,34 +1,41 @@
 <?php
-include "connection.php";
+include ("connection.php");
+
 $id_sewa = $_GET["id_sewa"];
+$biaya_sewa = $_GET["biaya_sewa_per_hari"];
 date_default_timezone_set('Asia/Jakarta');
 $tgl_kembali = date_create(date("Y-m-d H:i:s"));
 $tgl_kembali_fix = date("Y-m-d H:i:s");
-
-# selisih hari = selisih tgl_kembali dan tgl_sewa
-# jika selisiih hari > 7, maka denda = (selisih hari - 7) * 1
+# denda = selisih tgl_kembali dan tgl_sewa
+# jika selisih hari > 7, maka selisih hari - 7 * 1000
 # else denda = 0
 
 $sql = "select * from sewa where id_sewa='$id_sewa'";
+
 $hasil = mysqli_query($connect, $sql);
 $sewa = mysqli_fetch_array($hasil);
-$tgl_sewa = date_create($sewa["tgl_sewa"]);
 
-#menghitung selisih antara dua tanggal
+$tgl_sewa = date_create($sewa["tgl_sewa"]);
+#menghitung selisih 2 tanggal
 $selisih = date_diff($tgl_kembali, $tgl_sewa);
 # mengkonversi hasil selisih format jumlah hari
-$selisih_hari  = $selisih->format("%a");
-
-$biaya_sewa_per_hari = "select from mobil";
-$total_bayar = $selisih_hari * $biaya_sewa_per_hari;
+$selisih_hari = $selisih->format("%a");
 
 
-$sql = "insert into sewa values
-('','$id_sewa','$tgl_kembali_fix','$total_bayar')";
+if ($selisih_hari > 7) {
+    $denda = ($selisih_hari -7)*1000;
+}else {
+    $denda = 0;
+}
+
+$total_bayar = ($selisih_hari * $biaya_sewa)+$denda;
+
+$sql = "insert into pengembalian_mobil values
+('','$id_sewa','$tgl_kembali_fix','$denda','$total_biaya')";
 
 if (mysqli_query($connect, $sql)) {
-    header("location:list-sewa.php");
-} else {
+    header("Location:list-sewa.php");
+}else{
     echo mysqli_error($connect);
 }
 
